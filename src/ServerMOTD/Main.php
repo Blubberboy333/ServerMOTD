25c3eabbc6535b9a44fa01d6197e1d4bb09aeb89
Blubberboy333 <thecowmooman@gmail.com>
2015-05-27 11:15:58 -0400

    Update Main.php

--- src/ServerMOTD/Main.php
+++ src/ServerMOTD/Main.php
@@ -22,29 +22,44 @@ class Main extends PluginBase implements Listener{
   
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     if(strtolower($command->getName()) === "motd"){
-      if(!isset($args[0])){
-        $sender->sendMessage("Usage: /motd <read | set>");
-      }else{
-        if(strtolower($args[0]) == "read"){
-          $sender->sendMessage("Today's motd: " .$motd);
-        }elseif(strtolower($args[0]) == "set"){
-          if(!isset($args[1])){
-            $sender->sendMessage("Usage: motd set <message>");
-          }else{
-            $newmotd = implode($args[1]);
-            $motd[0] = $newmotd;
-            $broadcast = $this->getConfig()->get("Broadcast");
-            if($broadcast == "true"){
-              $message = $this->getConfig()->get("BroadcastMessage");
-              $this->getServer()->broadcastMessage($message);
-            }elseif($broadcast == "false"){
-              
+      if($sender->hasPermission("motd") || $sender->hasPermission("motd.command") $sender->hasPermission("motd.command.motd")){
+        if(!isset($args[0])){
+          $sender->sendMessage("Usage: /motd <read | set>");
+        }else{
+           if(strtolower($args[0]) == "read"){
+             if($sender->hasPermission("motd.command.read")){
+                $sender->sendMessage("Today's motd: " .$motd);
+             }else{
+               $sender->sendMessage("You don't have permission to do that!");
+             }
+          }elseif(strtolower($args[0]) == "set"){
+            if($sender->hasPermission("motd.command.set")){
+              if(!isset($args[1])){
+                $sender->sendMessage("Usage: motd set <message>");
+              }else{
+                $newmotd = implode($args[1]);
+                $motd[0] = $newmotd;
+                $broadcast = $this->getConfig()->get("Broadcast");
+                if($broadcast == "true"){
+                  $message = $this->getConfig()->get("BroadcastMessage");
+                  $this->getServer()->broadcastMessage($message);
+                }elseif($broadcast == "false"){
+                }else{
+                  $this->getLogger()->info(TextFormat::RED . "Error: Config not set up properly!!!");
+                }
+              }
             }else{
-              $this->getLogger()->info(TextFormat::RED . "Error: Config not set up properly!!!");
+              $sender->sendMessage("You don't have permission to do that!");
             }
           }
         }
+      }else{
+        $sender->sendMessage("You don't have permission to do that!");
       }
     }
+  }
+  public function onJoin(PlayerJoinEvent $event){
+    
+    $player = $event->getPlayer()->getDisplayName();
   }
 }
